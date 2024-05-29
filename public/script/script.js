16b23505ef365ccd7fefc12d8e7f73fd65d8093c
Eric <ericsouza94895@gmail.com>
2024-05-29 10:32:16 -0300

    feat: add functions GetCEP

--- public/script/script.js
+++ public/script/script.js
@@ -113,6 +113,81 @@ const ConsultAddress = () => {
     console.clear()
 }
 
+const GetCEP = () => {
+
+    let listCEP = []
+
+    const requests = new Request(`http://localhost:3000/address`, {
+        'method': 'GET',
+        'headers': {
+            'Content-Type': 'aplication/json'
+        }
+    })
+
+    fetch(requests)
+        .then(response => response.json())
+        .then(response => {
+
+            listCEP = response
+
+            console.log(listCEP)
+
+            if (!("erro" in response)) {
+
+                let table = document.getElementById('table-body');
+
+                let template = ""
+
+                listCEP.forEach(list => {
+
+                    template += `
+        
+        <tr>
+            
+            <td data-cell="cep">
+                ${list.cep}
+            </td>
+
+            <td data-cell="uf">
+                ${list.uf}
+            </td>
+
+            <td data-cell="publicplace">
+                ${list["public place"]}
+            </td>
+
+            <td data-cell="city">
+                ${list.city}
+            </td>
+
+            <td data-cell="neighborhood">
+                ${list.neighborhood}
+            </td>
+
+            <td data-cell="actions">
+                <button type="button" class="button green" onclick="updateUser(${list.id})">update</button>
+                <button type="button" class="button red" onclick="deleteUser(${list.id})">Delete</button>
+            </td>
+
+        </tr>
+          
+        `
+                });
+
+                table.innerHTML = template
+
+            } else {
+
+                alert('CEPs not found')
+                ResetForm()
+
+            }
+        })
+}
+
+window.addEventListener('DOMContentLoaded', GetCEP)
+
+
 const RegisterCEP = (address) => {
 
     fetch('http://localhost:3000/address', {
@@ -128,18 +203,17 @@ const RegisterCEP = (address) => {
 }
 
 const UpdateCEP = (address) => {
+    const { id, ...restOfAddress } = address;
 
-
-        fetch('http://localhost:3000/address', {
-            "method": "PATH",
-            "headers": {
-                "Content-type": "application/json"
-            },
-            "body": JSON.stringify(address)
-        }).then(resposta => {
-            resposta.ok ? window.alert('Endereço cadastrado!') : window.alert('Erro: ' + resposta.status)
-        })
-    
+    fetch(`http://localhost:3000/address/${id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(restOfAddress)
+    }).then(response => {
+        response.ok ? window.alert('Endereço atualizado!') : window.alert('Erro: ' + response.status)
+    });
 }
 
 
